f69afbddace61d617d66f7ddb9eeab0978c363c6
astradzhao <42476976+astradzhao@users.noreply.github.com>
2021-08-11 16:45:09 -0500

    redirects

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
+const CopyPlugin = require('copy-webpack-plugin')
 module.exports = {
     entry: './app/index.js',
     module: {
@@ -17,7 +18,10 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: 'app/index.html'
-        })
+        }),
+        new CopyPlugin([
+            { from: '_redirects'}
+        ])
     ], 
     mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
     devServer: {
